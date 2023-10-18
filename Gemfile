8eff14e7c1bebb367791c73d041efd4c399ae66d
Your Name <you@example.com>
2023-10-18 10:08:47 +1000

    add dotenv

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'bootstrap'
 # Form helpers
 gem 'simple_form'
 
+# Env management
+gem 'dotenv-rails'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << ENV.fetch('MAIN_HOST', nil)
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
